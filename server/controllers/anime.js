04ef1f7664ce94532bec1b2de79ccb41606b38f3
TyroneKong <tyronekong@gmail.com>
2022-06-08 15:43:00 +0100

    removed unecessary controller

--- server/controllers/anime.js
+++
@@ -1,7 +0,0 @@
-const axios = require("axios");
-
-exports.getAnime = (req, res) => {
-  axios.get(`https://api.jikan.moe/v4/?q=naruto&sfw`).then((response) => {
-    console.log(response.data);
-  });
-};
